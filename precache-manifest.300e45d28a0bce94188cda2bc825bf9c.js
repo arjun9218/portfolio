181df40c9c0568029ab0011cb446a8cda8300c79
Arjun G <nag9209@gmail.com>
2020-07-03 01:20:19 +0530

    Updates

---
+++ precache-manifest.300e45d28a0bce94188cda2bc825bf9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c664f218b6804d8190dfeb738ad898c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "375590a3da272098640a",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0104b02932a20bf6dc7",
+    "url": "/portfolio/static/js/2.2c671b78.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.2c671b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375590a3da272098640a",
+    "url": "/portfolio/static/js/main.124582c3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "bf583ca210bdc1488a246cf84e3b91e8",
+    "url": "/portfolio/static/media/profilePhoto.bf583ca2.JPG"
+  }
+]);
